8318f79359ac9023f9897ab9f7ad59279bbcbc8f
coder Bhaai <amit.khare588@gmail.com>
2024-03-17 23:11:49 +0530

    `commit`

---
+++ app/components/global/header.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Header(){
+    return <header>
+        <div className="container">
+            <img src="" alt="" />
+        </div>
+        <Link href="/">Home</Link>
+    </header>
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main><h1>hEELOW wORLD</h1></main>
+  );
+}
